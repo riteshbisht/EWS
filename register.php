976386bf8c2176ad07580f8e800d197d4aa37025
RITESH BISHT <ritesh.bisht93@gmail.com>
2015-03-27 22:45:05 +0530

    first commit

---
+++ register.php
@@ -0,0 +1,157 @@
+<html>
+<head>
+<title>
+EWS::REGISTER
+</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="jquery-1.11.1.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+
+<style>
+
+</style>
+</head>
+<body  id="registerbackground">
+		<div class="container-fluid">
+				<div class="row navbar">
+				  <div class="col-xs-12 ">
+				  		<nav >
+																 <div class="navbar-header pull-left">
+																      <a class="navbar-brand" href="home.html" style="font-size:40px;">EASY WAY SCIENCE</a>
+																  </div>
+				      												
+				      											<div class="pull-right">	
+																		  <ul class="nav nav-tabs">
+
+																		  <li role="presentation" ><a href="home.html">Home</a></li>
+																		  <li role="presentation"><a href="thisweek.html">This week's program</a></li>
+																		  <li role="presentation" class="active"><a href="#"> Register Now</a></li>
+																		  <li  role="presentation"><a href="contact.html">Contact Us</a></li>
+																		</ul>
+																	</div>
+													</nav>
+				  </div>
+				</div>
+				
+				<div class="row" >	
+					
+					<div class="col-xs-12">
+						<div class="row text-center" >
+							<h1>WORKSHOP REGISTRATION<h1>
+
+
+						</div>
+						
+						<div class="row">
+						<form data-toggle="validator" role="form">
+						
+							  <div class="col-xs-5">
+									  
+										<br>
+										 <div class="form-group">
+									<label for="inputName" class="control-label">Name</label>
+
+									  <input type="text" placeholder="First Name" maxlength="30" class="form-control" aria-label="..."  required></input>
+									  </div>
+									  
+
+									 
+									  <div class="form-group">
+									<label for="inputName" class="control-label">Last Name</label>
+									   <input type="text" placeholder="Last Name"  maxlength="30" class="form-control" aria-label="..."  required></input>
+									   </div>
+
+
+							 
+									    
+									  
+  									<div class="form-group">
+									<label for="inputName" class="control-label">School</label>
+									  <input type="text" class="form-control" maxlength="50" aria-label="..."placeholder="School"  required></input>
+									  </div>
+									    
+									  <br>
+									  	<div class="form-group">
+									<label for="inputName" class="control-label">Address</label>
+									   <textarea cols="30"  rows="5" style="resize:none;" class="form-control" aria-label="..." placeholder="Address"  required></textarea>
+									   </div>
+									    
+									  
+									  				<div class="form-group">
+									<label for="inputName" class="control-label">City</label>
+									   <input type="text" class="form-control" maxlength="20" aria-label="..."placeholder="City"  required></input>
+							           </div>
+							           <br>
+							  </div>
+
+							   <div class="col-xs-offset-1 col-xs-5">
+							   <br>
+ 										<div class="form-group">
+												<label for="inputName" class="control-label">Class</label>
+										 	  <select class="form-control" aria-label="..."  placeholder="Class"  required>
+												  <option value="KG">KG</option>
+												  <option value="First">1st</option>
+												  <option value="Second">2nd</option>
+												  <option value="Third">3rd</option>
+												  <option value="Fourth">4th</option>
+												  <option value="Fifth">5th</option>
+												  <option value="Sixth">6th</option>
+												  <option value="Seventh">7th</option>
+												  <option value="Eight">8th</option>
+												  <option value="Ninth">9th</option>
+												  <option value="Tenth">10th</option>
+												  <option value="Eleventh">11th</option>
+									 			 <option value="Twelveth">12th</option>
+									  
+									</select>
+									</div>
+									<br>
+										<div class="form-group">
+									<label for="inputName" class="control-label">	State/Province/Region</label>
+							   
+							 	  <select class="form-control" aria-label="..."  placeholder="State/Province/Region"  required>
+									  <option value="delhi">Delhi</option>
+									  <option value="haryana">Haryana</option>
+									  <option value="punjab">Punjab</option>
+									  <option value="maharashtra">Maharashtra</option>
+									    <option value="gujarat">Gujarat</option>
+									      <option value="rajasthan">Rajashthan</option>
+									        <option value="madhya preadesh">Madhya Pradesh</option>
+									          <option value="uttar pradesh">Uttar Pradesh</option>
+									            <option value="uttrakhand">Uttrakhand</option>
+									              <option value="bihar">Bihar</option>
+									</select>
+
+										</div>  
+							  	 <br>
+									  <br>
+											<div class="form-group">
+											     <label for="inputEmail" class="control-label">Email</label>
+												    <input type="email" maxlength="20" class="form-control" id="inputEmail" placeholder="Email" data-error="Bruh, that email address is invalid" required>
+												    <div class="help-block with-errors"></div>
+									      </div>
+                                  <br>
+									  <br>
+									  	<div class="form-group">
+											   <label for="inputEmail" class="control-label">Phone No.</label>
+											  <input type="text"  pattern="^([0-9]){10,}$" data-minlength="10"  class="form-control" aria-label="..." placeholder="Phoneno" data-error="10 digit phone no." required></input>
+											  	 <div class="help-block with-errors"></div>
+									  	 </div>
+									  <br><button type="submit" class="btn btn-primary">Submit</button>
+									  		<button type="reset" class="btn btn-primary">Reset</button>
+
+                               </div>
+						</div>
+						</form>
+					</div>
+				</div>
+
+		</div>	
+
+
+
+
+
+</body>
+</html>
